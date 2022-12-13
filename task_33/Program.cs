6e6bcf641c7b53b0137c11bc553eff0410ff70d9
VadimTalimonchik <tvadmins@gmail.com>
2022-12-13 11:40:38 +0300

    Задача 33

--- task_33/Program.cs
+++ task_33/Program.cs
@@ -4,4 +4,33 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
 
-Console.WriteLine("Hello, World!");
+int[] CreateArray()
+{
+    int[] array = new int[5];
+    for(int i = 0; i < 5; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+       Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine("\n");
+    return array;
+}
+
+void FindNum(int[] Array, int Num)
+{
+bool find = false;
+for(int i = 0; i <5; i++)
+{
+    if(Array[i] == Num) 
+    {
+        Console.WriteLine("Да");
+        find = true;
+        break;
+    }
+}
+if(find == false) Console.WriteLine("Нет");
+}
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+FindNum(CreateArray(), num);
+
