a84d255765f2556530d5dc91cefe7607f7a45c19
VadimTalimonchik <tvadmins@gmail.com>
2022-12-13 11:38:17 +0300

    Задача 32

--- task_32/Program.cs
+++ task_32/Program.cs
@@ -3,4 +3,24 @@
 // замените на соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2] 
 
-Console.WriteLine("Hello, World!");
+int[] CreateArray()
+{
+    int[] array = new int[4];
+    for(int i = 0; i < 4; i++)
+    {
+        array[i] = new Random().Next(-9, 9);
+       Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine("\n");
+    return array;
+}
+void LineNum(int[] Array)
+{
+for(int i=0; i<4; i++)
+{
+    Array[i] *= -1;
+    Console.Write($"{Array[i]} ");
+}
+}
+LineNum(CreateArray());
+
